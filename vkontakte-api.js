df8d54da6127cc27c49b05f6da4731537e15a337
kovalit <kovalit@mail.ru>
2016-08-17 12:57:18 +1000

    added: log

--- vkontakte-api.js
+++ vkontakte-api.js
@@ -1,10 +1,23 @@
 
     var nodemailer  = require('nodemailer');
     var config      = require('config');
-    
+    var log4js = require('log4js'); 
+
+    log4js.configure({
+        appenders: [
+          { 
+            "type": "file",
+            "filename": "logs/vk.log",
+            "category": "vk-api"
+          }
+        ]
+      });
+      
     var transporter = nodemailer.createTransport(
             config.get('smtp')
         );
+      
+    var log = log4js.getLogger("vk-api");
 
     var vk = {
         
@@ -21,9 +34,10 @@
             this.mailOptions.subject = 'Hello ' + user;
             
             transporter.sendMail(this.mailOptions, function(error, info){
-                if(error){
-                    return console.log(error);
+                if(error){  
+                    return log.error('email not send ' + user);
                 }
+                log.info('email send ' + user);
                 done();
             });
         } 
